a0d4db53a9480ce01387ec867910cda510cb0387
aryaun xames <ariankhamesi@gmail.com>
2024-02-21 23:12:30 +0330

    add bootstrap links

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+import './Styles/fonts.css'
+import './Styles/variables.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
