1f32594386d5555192e40b31016d2319e85b28a1
aryaun xames <ariankhamesi@gmail.com>
2024-02-21 23:10:01 +0330

    coding landing of header and styles

--- src/Component/Landing/Landing.jsx
+++ src/Component/Landing/Landing.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import "./Landing.css"
 
 export default function Landing() {
   return (
-    <div>Landing</div>
+    <div className='landing-container'>
+      <div className='landimg-text'>
+        <h1 className='landimg-text-h1'>The realm of movie lovers</h1>
+        <span className='landimg-text-span'> Find your taste in Empire Movie  </span>
+        <span className='landimg-text-span'>Create your wish list  </span>
+        <span className='landimg-text-span'> and interact with each other  </span>
+      </div>
+    </div>
   )
 }
