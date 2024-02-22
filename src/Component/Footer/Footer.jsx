28dd1a68f9682d38b6210822f5d440155c2655dc
aryaun xames <ariankhamesi@gmail.com>
2024-02-22 09:43:15 +0330

    fix some bugs

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Footer.css"
+
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer container">
+        <i class="fa-brands fa-telegram social-icon"></i>
+        <i class="fa-brands fa-instagram social-icon"></i>
+        <i class="fa-brands fa-twitter social-icon"></i>
+        <i class="fa-brands fa-facebook social-icon"></i>
+      </div>
+    </div>
+  )
+}
--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Header from '../Component/Header/Header'
+import Footer from '../Component/Footer/Footer'
+import MainBody from '../Component/MainBody/MainBody'
 
 export default function Index() {
   return (
-    <div>Index</div>
+    <div>
+    <Header/>
+    <MainBody/>
+    <Footer/>
+    </div>
   )
 }
