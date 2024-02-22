af511006cd4eea79cd3ab7d42fba49b41ab94304
aryaun xames <ariankhamesi@gmail.com>
2024-02-22 09:36:18 +0330

    coding user panel page and set classname

--- src/Pages/UserPanel/UserPanel.jsx
+++ src/Pages/UserPanel/UserPanel.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import "./UserPanel.css"
+import Topbar from "./../../Component/Header/Topbar"
 
 export default function UserPanel() {
   return (
-    <div>UserPanel</div>
+    <div className="user-panel-container">
+      <Topbar/>
+      <div className="account-panel">
+        <img src="" alt="" />
+        <div className="user-panel-form">
+        <form action="">
+          <input className='user-panel-input' type="text" placeholder='Full Name' value={""}/>
+          <input className='user-panel-input' type="text" placeholder='UserName' value={""}/>
+          <input className='user-panel-input' type="text" placeholder='Email' value={""}/>
+          <input className='user-panel-input' type="number" placeholder='Age' value={""} min={8} max={150}/>
+          <input className='user-panel-input' type="password" placeholder='Password' value={""}/>
+          <input className='user-panel-input' type="password" placeholder='Confirm Password' value={""}/>
+          <button className="user-panel-user">Sign Up</button>
+        </form>
+      </div>
+      </div>
+      <div className="playlist">
+      <div className="play-list-form">
+        <form action="">
+          <input className='play-list-input' type="text" placeholder='Movie Name' value={""}/>
+          <input className='play-list-input' type="text" placeholder='Time' value={""}/>
+          <input className='play-list-input' type="date" placeholder='Date' value={""}/>
+          <input className='play-list-input' type="text" placeholder='Overview' value={""} min={8} max={150}/>
+          <input className='play-list-input' type="range" placeholder='IMDB Score' value={""}/>
+          <input className='play-list-input' type="image" placeholder='Poster' value={""}/>
+          <button className="play-list-user">Add To Own List</button>
+        </form>
+      </div>
+      </div>
+    </div>
   )
 }
