df5aa94c760d6ae7daa53b2dfcefd4f6f1300f49
aryaun xames <ariankhamesi@gmail.com>
2024-02-22 10:57:12 +0330

    complete and styling register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -5,15 +5,17 @@ import Topbar from "./../../Component/Header/Topbar"
 export default function Register() {
   return (
     <div className="register-container">
+      <div className="gray"></div>
       <Topbar/>
       <div className="register-form">
-        <form action="">
-          <input className='register-input' type="text" placeholder='Full Name' value={""}/>
-          <input className='register-input' type="text" placeholder='UserName' value={""}/>
-          <input className='register-input' type="text" placeholder='Email' value={""}/>
-          <input className='register-input' type="number" placeholder='Age' value={""} min={8} max={150}/>
-          <input className='register-input' type="password" placeholder='Password' value={""}/>
-          <input className='register-input' type="password" placeholder='Confirm Password' value={""}/>
+        <label htmlFor="signIn" className='form-label'>Sign In</label>
+        <form action="signIn" className='signin-form'>
+          <input className='register-input' type="text" placeholder='Full Name' />
+          <input className='register-input' type="text" placeholder='UserName' />
+          <input className='register-input' type="text" placeholder='Email' />
+          <input className='register-input' type="number" placeholder='Age'  min={8} max={150}/>
+          <input className='register-input' type="password" placeholder='Password' />
+          <input className='register-input' type="password" placeholder='Confirm Password' />
           <button className="register-user">Sign Up</button>
         </form>
       </div>
