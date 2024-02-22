fdceb7bbdb45d6b55c89875cd92e79e2d873a4c0
aryaun xames <ariankhamesi@gmail.com>
2024-02-22 12:39:13 +0330

    coding cast page and styling

--- src/Pages/Cast/Cast.jsx
+++ src/Pages/Cast/Cast.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import "./Cast.css"
 import Topbar from "./../../Component/Header/Topbar"
 import Footer from '../../Component/Footer/Footer'
+import MovieBox from '../../Component/SliderBox/MovieBox'
+import { Link } from 'react-router-dom'
+import SliderTitle from '../../Component/SliderTitle/SliderTitle'
 
 export default function Cast() {
   return (
     <div className="cast-container">
       <div className="gray"></div>
-      <div><Topbar/></div>
+      <div><Topbar /></div>
       <div className="cast">
         <div className="cast-left">
-          <img src="/images/land-of-bad.png" alt="" className="cast-poster" />
+          <img src="/images/brad-pitt.jpg" alt="" className="cast-poster" />
           <div className="cast-score"></div>
         </div>
         <div className="cast-right">
@@ -20,14 +23,27 @@ export default function Cast() {
             <span className='role-cast'></span>
           </div>
           <div className="movie-description">
-            <h1 className='cast-title'>Land Of Bad</h1>
+            <h1 className='cast-title'>Brad Pitt</h1>
             <p className='cast-desc'><span className='biography'>Biography :</span> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nemo vitae beatae eius magnam minus blanditiis iure obcaecati et dolore! Adipisci qui voluptate corrupti sapiente laborum rerum recusandae quia. Quis.
-            Voluptatibus ipsam, explicabo magni aliquid tempora nostrum molestias ratione corrupti dolorum possimus quidem ipsa facere a quos omnis porro sit suscipit provident aliquam earum? Iure porro illo facilis odio quos!
-            Asperiores eos nemo at beatae quam suscipit, fuga, cum soluta mollitia aut itaque, repudiandae deleniti debitis cumque repellendus doloremque vero quaerat magnam ipsum! Fugit suscipit blanditiis dicta mollitia enim ipsam!</p>
+              Voluptatibus ipsam, explicabo magni aliquid tempora nostrum molestias ratione corrupti dolorum possimus quidem ipsa facere a quos omnis porro sit suscipit provident aliquam earum? Iure porro illo facilis odio quos!
+              Asperiores eos nemo at beatae quam suscipit, fuga, cum soluta mollitia aut itaque, repudiandae deleniti debitis cumque repellendus doloremque vero quaerat magnam ipsum! Fugit suscipit blanditiis dicta mollitia enim ipsam!</p>
           </div>
         </div>
       </div>
-      <div className="cast-movie"></div>
+      <div className="container zIndex">
+        <SliderTitle
+          title={"Movies"}
+          btn={false}
+        />
+      </div>
+      <div className="cast-movie">
+        <Link className='router-link' to={"/movie-info/movieName"}><MovieBox poster={"vert"} /></Link>
+        <Link className='router-link' to={"/movie-info/movieName"}><MovieBox poster={"vert"} /></Link>
+        <Link className='router-link' to={"/movie-info/movieName"}><MovieBox poster={"vert"} /></Link>
+        <Link className='router-link' to={"/movie-info/movieName"}><MovieBox poster={"vert"} /></Link>
+        <Link className='router-link' to={"/movie-info/movieName"}><MovieBox poster={"vert"} /></Link>
+        <Link className='router-link' to={"/movie-info/movieName"}><MovieBox poster={"vert"} /></Link>
+      </div>
     </div>
   )
 }
