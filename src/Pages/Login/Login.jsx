247862b53534e16a02eaf094a18c4430e8bae93e
aryaun xames <ariankhamesi@gmail.com>
2024-02-22 09:40:24 +0330

    coding login page and set classname

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import "./Login.css"
+import Topbar from "./../../Component/Header/Topbar"
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className="login-container">
+      <Topbar/>
+      <div className="login-form">
+        <form action="">
+          <input className='login-input' type="text" placeholder='UserName' value={""}/>
+          <input className='login-input' type="password" placeholder='Password' value={""}/>
+          <button className="login-user">Sign In</button>
+        </form>
+      </div>
+    </div>
   )
 }
