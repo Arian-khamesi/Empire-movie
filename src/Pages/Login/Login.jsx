ad8e5e5efe829496222c9b4f928dd0cae0416c76
aryaun xames <ariankhamesi@gmail.com>
2024-02-22 10:58:05 +0330

    complete and styling login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,12 +5,14 @@ import Topbar from "./../../Component/Header/Topbar"
 export default function Login() {
   return (
     <div className="login-container">
+      <div className="gray2"></div>
       <Topbar/>
       <div className="login-form">
-        <form action="">
-          <input className='login-input' type="text" placeholder='UserName' value={""}/>
-          <input className='login-input' type="password" placeholder='Password' value={""}/>
-          <button className="login-user">Sign In</button>
+      <label htmlFor="signIn" className='form-label'>Log In</label>
+        <form action="signIn" className='signin-form'>
+          <input className='login-input' type="text" placeholder='UserName'/>
+          <input className='login-input' type="password" placeholder='Password'/>
+          <button className="login-user">Log In</button>
         </form>
       </div>
     </div>
