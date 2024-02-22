f2b8605e372c19e63b8495344ce158222434ad35
aryaun xames <ariankhamesi@gmail.com>
2024-02-22 09:42:56 +0330

    clebriti component styles

---
+++ src/Component/Celebriti/Celebriti.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Celebriti.css"
+
+export default function Celebriti() {
+  return (
+    <div className="CelebContainer">
+        <img src="images/brad-pitt.jpg" alt="actor" className='actor-img'/>
+        <span className='actor-name'>Brad Pitt</span>
+    </div>
+  )
+}
+
