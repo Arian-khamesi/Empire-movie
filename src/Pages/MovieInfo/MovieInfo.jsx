a6a60232f99585803cf4f44411c3e9fe39ee7a2b
aryaun xames <ariankhamesi@gmail.com>
2024-02-24 12:12:02 +0330

    create new rout for show main movie and series (dynamic)

--- src/Pages/MovieInfo/MovieInfo.jsx
+++ src/Pages/MovieInfo/MovieInfo.jsx
@@ -2,28 +2,38 @@ import React from 'react'
 import "./MovieInfo.css"
 import Topbar from "./../../Component/Header/Topbar"
 import Footer from '../../Component/Footer/Footer'
+import { useParams } from 'react-router-dom'
+import {movies} from '../../DataBase/data'
+import { series } from '../../DataBase/data'
 
 export default function MovieInfo() {
+
+  const { movieId } = useParams()
+  const {movieType } = useParams()
+  console.log(movieId);
+  console.log(movieType);
+
+  const mainMovie = movies.find(movie => movie.id == movieId)
+  const mainSerie = series.find(serie => serie.id == movieId)
+
   return (
-    <div className="movie-info-container">
+    <div className="movie-info-container" style={movieType==="movie"?{backgroundImage: `url(${mainMovie.img})`}:{backgroundImage: `url(${mainSerie.img})`}}>
       <div className="gray-movie-info"></div>
-      <div><Topbar gradiant={true}/></div>
+      <div className='topbar-z'><Topbar gradiant={true} /></div>
       <div className="movie-info">
         <div className="movie-info-left">
-          <img src="/images/land-of-bad.png" alt="" className="movie-info-poster" />
-          <div className="movie-info-score"></div>
+          <img src={movieType==="movie"?mainMovie.img:mainSerie.img} alt="" className="movie-info-poster" />
+          {/* <div className="movie-info-score"></div> */}
         </div>
         <div className="movie-info-right">
           <div className="movie-information">
-            <span className='time-movie'></span>
-            <span className='date-movie'></span>
-            <span className='score-imdb-movie'></span>
+            <span className='time-movie'>{movieType==="movie"?"Time":"Season"} : {movieType==="movie"?mainMovie.time:mainSerie.season}</span>
+            <span className='date-movie'>Year : {movieType==="movie"?mainMovie.year:mainSerie.year}</span>
+            <span className='score-imdb-movie'>IMDB : {movieType==="movie"?mainMovie.score:mainSerie.score}</span>
           </div>
           <div className="movie-description">
-            <h1 className='movie-info-title'>Land Of Bad</h1>
-            <p className='movie-info-desc'><span className='overview'>Overview :</span> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nemo vitae beatae eius magnam minus blanditiis iure obcaecati et dolore! Adipisci qui voluptate corrupti sapiente laborum rerum recusandae quia. Quis.
-            Voluptatibus ipsam, explicabo magni aliquid tempora nostrum molestias ratione corrupti dolorum possimus quidem ipsa facere a quos omnis porro sit suscipit provident aliquam earum? Iure porro illo facilis odio quos!
-            Asperiores eos nemo at beatae quam suscipit, fuga, cum soluta mollitia aut itaque, repudiandae deleniti debitis cumque repellendus doloremque vero quaerat magnam ipsum! Fugit suscipit blanditiis dicta mollitia enim ipsam!</p>
+            <h1 className='movie-info-title'>{movieType==="movie"?mainMovie.name:mainSerie.name}</h1>
+            <p className='movie-info-desc'><span className='overview'>Overview :</span>{movieType==="movie"?mainMovie.desc:mainSerie.desc}</p>
           </div>
         </div>
       </div>
