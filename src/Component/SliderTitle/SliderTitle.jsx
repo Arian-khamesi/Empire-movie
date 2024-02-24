7aedbe0697c11b45d61cf5a2d70218a864b12924
aryaun xames <ariankhamesi@gmail.com>
2024-02-24 12:16:53 +0330

    fixed slider title bugs

--- src/Component/SliderTitle/SliderTitle.jsx
+++ src/Component/SliderTitle/SliderTitle.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./SliderTitle.css"
+import { Link } from 'react-router-dom'
 
-export default function SliderTitle({title,btn,margin}) {
+export default function SliderTitle({ title, btn, margin, href }) {
   return (
-    <div className={margin?"slider-title-container addMargin":"slider-title-container"}>
-        <h2 className='slider-title'>{title}</h2>
-       {btn? <button className='slider-all'>See All</button>:null}
+    <div className={margin ? "slider-title-container addMargin" : "slider-title-container"}>
+      <h2 className='slider-title'>{title}</h2>
+      {btn ? <Link to={`/show-all/${href}/1`}><button className='slider-all'>See All</button></Link> : null}
     </div>
   )
 }
