86068c19591571915e0cf7643f00ccfd057bebe7
aryaun xames <ariankhamesi@gmail.com>
2024-02-24 12:08:21 +0330

    build a component for show movies in slider

--- src/Component/SliderBox/MovieBox.jsx
+++ src/Component/SliderBox/MovieBox.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import "./MovieBox.css"
+import { Link } from 'react-router-dom'
 
-export default function MovieBox({poster}) {
+export default function MovieBox({ poster, name, score, img,id,series}) {
   return (
-    <div className={poster==="horiz"?"horiz-movie-box":"vert-movie-box"}>
-        <div className={poster==="horiz"?"horiz-cover-movie-box":"vert-cover-movie-box"}>
-            <img src={poster==="horiz"?"/images/land-of-bad.png":"/images/house-of-dragon.jpg"} alt="" className={poster==="horiz"?"horiz-img-movie-box":"vert-img-movie-box"} />
-            <div className={poster==="horiz"?"horiz-movie-score":"vert-movie-score"}>8</div>
+    <Link className='router-link' to={series?`/movie-info/series/${id}`:`/movie-info/movie/${id}`}>
+      <div className={poster === "horiz" ? "horiz-movie-box" : "vert-movie-box"}>
+        <div className={poster === "horiz" ? "horiz-cover-movie-box" : "vert-cover-movie-box"}>
+          <img src={img} alt="" className={poster === "horiz" ? "horiz-img-movie-box" : "vert-img-movie-box"} />
+          <div className={poster === "horiz" ? "horiz-movie-score" : "vert-movie-score"}>{score}</div>
         </div>
-        <span className={poster==="horiz"?"horiz-title-movie-box":"vert-title-movie-box"}>{poster==="horiz"?"Land Of Bad":"House Of Dragon"}</span>
-    </div>
+        <span className={poster === "horiz" ? "horiz-title-movie-box" : "vert-title-movie-box"}>{name}</span>
+      </div>
+    </Link>
   )
 }
